e2d5e3e8764330ad88a8432131b2b0ce7f7623e0
bakkura <dupreyrw@gmail.com>
2024-03-05 20:40:12 -0800

    added bg and player

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Game Name")
 
-BG = pygame.image.load("")
+BG = pygame.image.load("space.jpeg")
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+def draw(player):
+    WIN.blit(BG, (0, 0))
+
+    pygame.draw.rect(WIN, (200, 0, 100), player)
+
+    pygame.display.update()
 
 def main():
     run = True
     
+    player = pygame.Rect(WIDTH / 2, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+        draw(player)
 
     pygame.quit()
 
