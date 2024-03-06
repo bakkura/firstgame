03c79cf2820d7f01396d6373e46ceb500d36d842
bakkura <dupreyrw@gmail.com>
2024-03-05 20:56:17 -0800

    added player movement and timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -11,8 +12,15 @@ BG = pygame.image.load("space.jpeg")
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
 
-def draw(player):
+PLAYER_VEL = 5
+
+FONT = pygame.font.SysFont("arial", 50)
+
+def draw(player, elapsed_time):
     WIN.blit(BG, (0, 0))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}", 1, "white")
+    WIN.blit(time_text, (10, 10))
 
     pygame.draw.rect(WIN, (200, 0, 100), player)
 
@@ -23,12 +31,25 @@ def main():
     
     player = pygame.Rect(WIDTH / 2, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
 
+    clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
     while run:
+        clock.tick(60)
+        elapsed_time = time.time() - start_time
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw(player)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL + player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        
+        draw(player, elapsed_time)
 
     pygame.quit()
 
